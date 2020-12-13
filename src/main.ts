f8cc3ce9dfbaeaf90f9a94ea1a1bb1d53f37b575
Vik <valakar@gmail.com>
2020-12-13 18:40:49 +0200

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: (req, callback) => callback(null, true),
+    origin: '*',
   });
   app.use(helmet());
 
